0c0d2fcb96feaba536fb5285e54d2863f98926ec
task650 <task650@gmail.com>
2013-01-04 12:01:21 -0800

    Add build script, mkbootimg, and mkbootfs

---
+++ build_d2att_aosp.sh
@@ -0,0 +1,71 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+export PARENT_DIR=`readlink -f ..`
+export INITRAMFS_DEST=$KERNELDIR/kernel/usr/initramfs
+export INITRAMFS_SOURCE=`readlink -f ..`/Kernel_Stuff/Ramdisks/AOSP_JB_MR1
+export CONFIG_AOSP_BUILD=y
+export PACKAGEDIR=$PARENT_DIR/Kernel_Stuff/Packages_Underwear_4.2/AOSP_JB_ATT
+#Enable FIPS mode
+export USE_SEC_FIPS_MODE=true
+export ARCH=arm
+# export CROSS_COMPILE=/home/task650/CM10_JB/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+# export CROSS_COMPILE=/home/task650/CM10_JB/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+export CROSS_COMPILE=$PARENT_DIR/linaro4.7/bin/arm-eabi-
+
+echo "Remove old Package Files"
+rm -rf $PACKAGEDIR/*
+
+echo "Setup Package Directory"
+mkdir -p $PACKAGEDIR/system/lib/modules
+
+echo "Create initramfs dir"
+mkdir -p $INITRAMFS_DEST
+
+echo "Remove old initramfs dir"
+rm -rf $INITRAMFS_DEST/*
+
+echo "Copy new initramfs dir"
+cp -R $INITRAMFS_SOURCE/* $INITRAMFS_DEST
+
+echo "chmod initramfs dir"
+chmod -R g-w $INITRAMFS_DEST/*
+rm $(find $INITRAMFS_DEST -name EMPTY_DIRECTORY -print)
+rm -rf $(find $INITRAMFS_DEST -name .git -print)
+
+echo "Remove old zImage"
+rm $PACKAGEDIR/zImage
+rm arch/arm/boot/zImage
+
+echo "Make the kernel"
+make underwear_d2_defconfig
+
+HOST_CHECK=`uname -n`
+if [ $HOST_CHECK = 'ktoonsez-VirtualBox' ] || [ $HOST_CHECK = 'task650-Underwear' ]; then
+	echo "Ktoonsez/task650 24!"
+	make -j24
+else
+	echo "Others! - " + $HOST_CHECK
+	make -j`grep 'processor' /proc/cpuinfo | wc -l`
+fi;
+
+echo "Copy modules to Package"
+cp -a $(find . -name *.ko -print |grep -v initramfs) $PACKAGEDIR/system/lib/modules/
+
+if [ -e $KERNELDIR/arch/arm/boot/zImage ]; then
+	echo "Copy zImage to Package"
+	cp arch/arm/boot/zImage $PACKAGEDIR/zImage
+
+	echo "Make boot.img"
+	./mkbootfs $INITRAMFS_DEST | gzip > $PACKAGEDIR/ramdisk.gz
+	./mkbootimg --cmdline 'console = null androidboot.hardware=qcom user_debug=31 zcache' --kernel $PACKAGEDIR/zImage --ramdisk $PACKAGEDIR/ramdisk.gz --base 0x80200000 --pagesize 2048 --ramdiskaddr 0x81500000 --output $PACKAGEDIR/boot.img 
+	export curdate=`date "+%m-%d-%Y"`
+	cd $PACKAGEDIR
+	cp -R ../META-INF .
+	rm ramdisk.gz
+	rm zImage
+	rm ../Underwear-kernel-4.2-task650*.zip
+	zip -r ../Underwear-kernel-4.2-task650-$curdate.zip .
+	cd $KERNELDIR
+else
+	echo "KERNEL DID NOT BUILD! no zImage exist"
+fi;
